6c943b7b5ef143e5ede48eb581e97ed3b59fd9b5
Tim Bowland <tbowland@outlook.com>
2017-09-30 22:52:09 -0600

    Adding solution

---
+++ CodinGame/ChuckNorris.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame
+{
+	class ChuckNorris
+	{
+		public static void Unary()
+		{
+			while (true)
+			{
+				Console.WriteLine("Enter phrase to encode with Unary.");
+				string message = Console.ReadLine();
+
+				if (message.Equals("exit"))
+					break;
+
+				List<int> binary = new List<int>();
+
+				foreach (char c in message)
+				{
+					var a = (int)c;
+					string b = string.Concat("0000000", Convert.ToString(a, 2));
+					binary.AddRange(b.Substring(b.Length - 7).ToCharArray().Select(x => (int)x).ToList());
+				}
+
+				int previous = -2;
+
+				string output = string.Empty;
+
+				binary.ForEach(current =>
+				{
+					if (current != previous)
+					{
+						if (current == 49)
+							output = string.Concat(output, " 0 ");
+						else
+							output = string.Concat(output, " 00 ");
+
+						previous = current;
+					}
+
+					output = string.Concat(output, "0");
+				});
+
+				Console.WriteLine(output.Trim());
+			}
+		}
+	}
+}
---
+++ CodinGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Select the program to run.");
+			string program = Console.ReadLine();
+			
+			switch(program)
+			{
+				case "ChuckNorris":
+					ChuckNorris.Unary();
+					break;
+
+				default:
+					Console.WriteLine("Program not found.");
+					break;
+			}
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadLine();
+		}
+	}
+}
